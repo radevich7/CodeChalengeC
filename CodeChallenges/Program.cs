226e573f01237c77f3f913c40c159bf9223e39dd
Julian Radevych <jradevych@munisight.com>
2022-05-20 17:51:28 -0600

    <Challange1>

---
+++ CodeChallenges/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace CodeChallenges
+{
+    class Tests
+    {
+        public static int DivisibleBy(int a, int b)
+        {
+            public static int Sum(int n)
+            {
+
+            }
+
+
+            return a;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Tests.Sum(5));
+
+            Console.ReadKey();
+        }
+
+    }
+
+}
