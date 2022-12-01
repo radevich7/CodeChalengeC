ad52cb357333dfcdda35c99fa3484315c1bb8885
Julian Radevych <jradevych@munisight.com>
2022-12-01 16:01:56 -0700

    <Build: array sorting  >

--- CodeChallenges/Program.cs
+++ CodeChallenges/Program.cs
@@ -9,71 +9,45 @@ using System.Text.RegularExpressions;
 
 namespace CodeChallenges
 {
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
-        }
-    }
-
+ 
     class Tests
     {
+   
 
-
-        public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        static int RemoveElement(int[] nums, int val)
         {
-            if (list1 == null)
-                return list2;
-            if (list2 == null)
-                return list1;
-            ListNode newHead = new ListNode();
-            var result = newHead;
-            while (list1 != null && list2!=null)
+            if (nums.Length == 0)
+                return 0;
+
+            int counter = 0;
+            for (int i = 0; i < nums.Length; i++)
             {
-
-                if (list1.val >=list2.val)
+                if (nums[i] == val)
                 {
-                    result.next = list2;
-                    list2 = list2.next;
-                }
-                else
-                {
-                    result.next = list1;
-                    list1 = list1.next;
+                    nums[i] = int.MaxValue;
+                    counter++;
                 }
 
-
-
-                result = result.next;
-              
             }
 
-            if (list1 != null) result.next = list1;
-            if (list2 != null) result.next = list2;
+            Array.Sort(nums);
 
-            return newHead.next;
+
+            foreach (var item in nums)
+            {
+                Console.WriteLine(item) ;
+            }
+            return nums.Length-counter;
+
         }
         class Program
         {
 
             static void Main(string[] args)
             {
-                ListNode list1 = new ListNode();
-                list1.val =2;
-                list1.next = new ListNode(3);
-                list1.next.next = new ListNode(5);
 
 
-                ListNode list2 = new ListNode();
-                list2.val = 4;
-                list2.next = new ListNode(3);
-                list2.next.next = new ListNode(6);
-
-                Tests.MergeTwoLists(list1, list2);
+                Console.WriteLine(Tests.RemoveElement( new int[] { 3,2,2,3}, 2));
 
                 Console.ReadKey();
             }
