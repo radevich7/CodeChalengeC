5e9f7c20d335e1108a39805cce5143c4a6183b42
Julian Radevych <jradevych@munisight.com>
2022-12-01 09:18:25 -0700

    <Build: Node challenge>

--- CodeChallenges/Program.cs
+++ CodeChallenges/Program.cs
@@ -9,24 +9,71 @@ using System.Text.RegularExpressions;
 
 namespace CodeChallenges
 {
-   
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
     class Tests
     {
 
-        public static double[] UniqueSort(double[] arr)
+
+        public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
         {
-               double[] uniqueNumbers= arr.Distinct().ToArray();
+            if (list1 == null)
+                return list2;
+            if (list2 == null)
+                return list1;
+            ListNode newHead = new ListNode();
+            var result = newHead;
+            while (list1 != null && list2!=null)
+            {
 
-            return 
+                if (list1.val >=list2.val)
+                {
+                    result.next = list2;
+                    list2 = list2.next;
+                }
+                else
+                {
+                    result.next = list1;
+                    list1 = list1.next;
+                }
+
+
+
+                result = result.next;
+              
+            }
+
+            if (list1 != null) result.next = list1;
+            if (list2 != null) result.next = list2;
+
+            return newHead.next;
         }
-
         class Program
         {
 
             static void Main(string[] args)
             {
+                ListNode list1 = new ListNode();
+                list1.val =2;
+                list1.next = new ListNode(3);
+                list1.next.next = new ListNode(5);
 
-                Console.WriteLine(Tests.UniqueSort(new double[] { 1, 5, 8, 2, 3, 4, 4, 4, 10 }));
+
+                ListNode list2 = new ListNode();
+                list2.val = 4;
+                list2.next = new ListNode(3);
+                list2.next.next = new ListNode(6);
+
+                Tests.MergeTwoLists(list1, list2);
 
                 Console.ReadKey();
             }
---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,66 @@
+﻿
+
+
+
+namespace LinkedList
+{
+    internal class Node
+    {
+        internal int data;
+        internal Node next;
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+
+        internal void InsertFront(SingleLinkedList singleList, int newData)
+        {
+            Node newNode = new Node(newData);
+            newNode.next = singleList.head;
+            singleList.head = newNode;
+
+        }
+    }
+
+
+
+    internal class DoubleNode
+    {
+        internal int data;
+        internal DoubleNode next;
+        internal DoubleNode previous;
+
+        public DoubleNode(int d)
+        {
+            data = d;
+            previous = null;
+            next = null;
+        }
+
+        internal void InsertFront(DoubleLinkedList doubleList, int newData)
+        {
+            DoubleNode newNode = new DoubleNode(newData);
+            newNode.next = doubleList.head;
+            newNode.previous = null;
+            if(doubleList.head != null)
+            {
+                doubleList.head.next = newNode;
+            }
+            doubleList.head = newNode;
+
+        }
+    }
+
+    internal class SingleLinkedList
+    {
+        internal Node head;
+    }
+    internal class DoubleLinkedList
+    {
+        internal DoubleNode head;
+    }
+
+
+
+}
