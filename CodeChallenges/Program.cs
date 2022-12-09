fd1ee396ecbc5c90b99d0363d63177a4283c5a33
Julian Radevych <radevich7@gmail.com>
2022-12-09 16:34:25 -0700

    <Build: Longets Substring without repeating chalange set up>

--- CodeChallenges/Program.cs
+++ CodeChallenges/Program.cs
@@ -7,106 +7,63 @@ using System.IO;
 using System.Globalization;
 using System.Text.RegularExpressions;
 using System.Collections;
+using CodeChallenges;
 
 namespace CodeChallenges
 {
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
-        }
-    }
-
     class Tests
     {
 
-        static public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        static public int LengthOfLongestSubstring(string s)
         {
-
-
-            var left = 0;
-            var list = new Stack();
-
-            while (l1 != null || l2 != null)
+            if(s.Length)
             {
-                var first = l1 == null ? 0 : l1.val;
-                var second = l2 == null ? 0 : l2.val;
-
-
-                var add = (first + second + left);
-
-                if (add > 9)
-                {
-                    left = Convert.ToInt32(add.ToString().Substring(0, 1));
-                }
-                else
-                {
-                    left = 0;
-                }
-
-
-                list.Push(add % 10);
-
-
-                if (l1 == null && l2 != null)
-                {
-                    l1 = null;
-                    l2 = l2.next;
-                }
-                else if (l1 != null && l2 == null)
-                {
-                    l1 = l1.next;
-                    l2 = null;
-                }
-                else
-                {
-                    l1 = l1.next;
-                    l2 = l2.next;
-                }
-
+                return 1;
             }
-            if (left > 0)
+            Dictionary<char, int> map = new Dictionary<char, int>();
+
+            char[] array = s.ToCharArray();
+            int maximumChar = 0;
+
+            for (int i = 0; i < array.Length; i++)
             {
-                list.Push(left);
+                try
+                {
+                    map.Add(array[i], 0);
+                    maximumChar++;
+
+                }
+                catch (Exception)
+                {
+                maximumChar=maximumChar>map.Count ? maximumChar : map.Count;
+                    i--;
+
+                    map.Clear();
+                }
             }
-
-            var returnNode = new ListNode((int)list.Pop());
-            while (list.Count > 0)
-            {
-
-                returnNode = new ListNode((int)list.Pop(), returnNode);
-            }
-
-
-
-
-
-            return returnNode;
+            return maximumChar;
 
         }
 
-
-
-
     }
-    class Program
+
+
+
+
+}
+class Program
+{
+
+    static void Main(string[] args)
     {
 
-        static void Main(string[] args)
-        {
-            var l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
-            var l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
 
 
 
-            Console.WriteLine(Tests.AddTwoNumbers(l1, l2));
+        Console.WriteLine(Tests.LengthOfLongestSubstring("pwwkew"));
 
-            Console.ReadKey();
-        }
-
+        Console.ReadKey();
     }
+
 }
+
