01690db694c5b3e37ace6bbd149d527edd78a640
Julian Radevych <radevich7@gmail.com>
2023-01-06 13:47:11 -0700

    <Longest palindromic substring, not finished>

--- CodeChallenges/Program.cs
+++ CodeChallenges/Program.cs
@@ -8,35 +8,52 @@ using System.Globalization;
 using System.Text.RegularExpressions;
 using System.Collections;
 using CodeChallenges;
+using System.ComponentModel;
 
 namespace CodeChallenges
 {
     class Tests
     {
 
-        static public int LengthOfLongestSubstring(string s)
+        static public string LongestPalindrome(string s)
         {
-            if (s == null || s == String.Empty)
-                return 0;
+            if (s.Length <= 1 && String.IsNullOrEmpty(s)) return s;
+            int start = 0;
+            int end = 0;
 
-            HashSet<char> set = new HashSet<char>();
-            int currentMax = 0,
-                i = 0,
-                j = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                int length1 = Expand(s, i, i);
+                int length2 = Expand(s, i, i + 1);
 
-            while (j < s.Length)
-                if (!set.Contains(s[j]))
+                int length = Math.Max(length1, length2);
+
+                if (length > end - start)
                 {
-                    set.Add(s[j++]);
-                    currentMax = Math.Max(currentMax, j - i);
+                    start = i - ((length - 1) / 2);
+                    end = i + (length / 2);
                 }
-                else
-                    set.Remove(s[i++]);
 
-            return currentMax;
+
+            }
+            return s.Substring(start, end + 1);
+
 
         }
+        static public int Expand(string s, int left, int right)
+        {
+            if (s == null || left > right) return 0;
 
+            while (left >= 0 && right < s.Length && s[left] == s[right])
+            {
+                left--;
+                right++;
+            }
+
+            return right - left - 1;
+
+
+        }
     }
 
 
@@ -52,7 +69,7 @@ class Program
 
 
 
-        Console.WriteLine(Tests.LengthOfLongestSubstring("abcbbcbb"));
+        Console.WriteLine(Tests.LongestPalindrome("babad"));
 
         Console.ReadKey();
     }
